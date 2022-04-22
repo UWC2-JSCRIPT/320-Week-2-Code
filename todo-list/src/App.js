712ca6669bf965e23318c0c05b7ccfb531983c78
Brian Hague <bhague1281@gmail.com>
2022-04-21 19:33:34 -0700

    initial commit

---
+++ todo-list/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Picture from './Picture';
+
+function App() {
+  return (
+    <div>
+      {/* this is a comment */}
+      {/* <Picture
+        src="https://media.giphy.com/media/tIoeLjCZ1Ib3a/giphy.gif"
+        alt="Picture of Dave Grohl being sassy"
+      /> */}
+    </div>
+  );
+}
+
+export default App;
